136215228ab4deea4323e2c3a5268eb608431843
Chris Siku <chrissiku5@gmail.com>
2022-12-15 17:06:55 +0200

    Fix voice

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable prefer-destructuring */
-const text = document.querySelector('.quote');
-const button = document.querySelector('button');
-const author = document.querySelector('.name');
-const voice = document.querySelector('.speech');
-const copy = document.querySelector('.copy');
-const twitter = document.querySelector('.twitter');
+const text = document.querySelector(".quote");
+const button = document.querySelector("button");
+const author = document.querySelector(".name");
+const voice = document.querySelector(".speech");
+const copy = document.querySelector(".copy");
+const twitter = document.querySelector(".twitter");
 const synthesis = speechSynthesis;
 
 const generateQuote = () => {
-  button.classList.add('loading');
-  button.innerText = 'Loading ...';
-  fetch('http://api.quotable.io/random')
+  button.classList.add("loading");
+  button.innerText = "Loading ...";
+  fetch("http://api.quotable.io/random")
     .then((response) => response.json())
     .then((result) => {
       text.innerText = result.content;
       author.innerText = result.author;
-      button.classList.remove('loading');
-      button.innerHTML = 'New Quote';
+      button.classList.remove("loading");
+      button.innerHTML = "New Quote";
     });
 };
 
-copy.addEventListener('click', () => {
+copy.addEventListener("click", () => {
   navigator.clipboard.writeText(text.innerText);
-  alert('Text copied successfully');
+  alert("Text copied successfully");
 });
 
-twitter.addEventListener('click', () => {
+twitter.addEventListener("click", () => {
   const url = `https://twitter.com/intent/tweet?url=${text.innerText}`;
-  window.open(url, '_blank');
+  window.open(url, "_blank");
 });
 
-voice.addEventListener('click', () => {
-  if (!text.classList.contains('loading')) {
-    const [voices] = window.speechSynthesis.getVoices();
+voice.addEventListener("click", () => {
+  if (!text.classList.contains("loading")) {
+    const voices = window.speechSynthesis.getVoices();
     const enunciation = new SpeechSynthesisUtterance(
-      `${text.innerText} by ${author.innerText}`,
+      `${text.innerText} by ${author.innerText}`
     );
     enunciation.voice = voices[6];
     synthesis.speak(enunciation);
     setInterval(() => {
       !synthesis.speaking
-        ? voice.classList.remove('active')
-        : voice.classList.add('active');
+        ? voice.classList.remove("active")
+        : voice.classList.add("active");
     }, 10);
   }
 });
 
 generateQuote();
-button.addEventListener('click', generateQuote);
+button.addEventListener("click", generateQuote);
