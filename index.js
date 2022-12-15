b66b3b3a26c75a08ee2b5119ff6e87cdba771336
Chris Siku <chrissiku5@gmail.com>
2022-12-15 16:24:57 +0200

    Change voice

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const author = document.querySelector(".name");
 const voice = document.querySelector(".speech");
 const copy = document.querySelector(".copy");
 const twitter = document.querySelector(".twitter");
+const synthesis = speechSynthesis;
 
 const generateQuote = () => {
   button.classList.add("loading");
@@ -26,6 +27,22 @@ copy.addEventListener("click", () => {
 twitter.addEventListener("click", () => {
   let url = `https://twitter.com/intent/tweet?url=${text.innerText}`;
   window.open(url, "_blank");
+});
+
+voice.addEventListener("click", () => {
+  if (!text.classList.contains("loading")) {
+    let voices = window.speechSynthesis.getVoices();
+    let enunciation = new SpeechSynthesisUtterance(
+      `${text.innerText} by ${author.innerText}`
+    );
+    enunciation.voice = voices[6];
+    synthesis.speak(enunciation);
+    setInterval(() => {
+      !synthesis.speaking
+        ? voice.classList.remove("active")
+        : voice.classList.add("active");
+    }, 10);
+  }
 });
 
 generateQuote();
